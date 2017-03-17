a795898a4c137a4b8935ff7c99aff42e91cd040e
Daniele <danitava@libero.it>
2017-03-17 17:53:23 +0100

    Aggiunta Classi

---
+++ Progetto/src/progetto/LetturaOggetti.java
@@ -0,0 +1,20 @@
+package progetto;
+import java.io.*;
+import java.util.ArrayList;
+
+public class LetturaOggetti 
+{
+    private String nomefile;
+    private BufferedReader inputStream;
+    
+    public LetturaOggetti(String nfile)
+    {
+        nomefile=nfile;
+    }
+    
+    public ArrayList<Oggetto> lettura()
+    {
+        return null;
+    }
+    
+}
