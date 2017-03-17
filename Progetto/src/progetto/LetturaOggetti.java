1c278f86b2ee80b1f9e64f2eeadd1a46a41aa86c
Daniele <danitava@libero.it>
2017-03-17 18:01:46 +0100

    Creazione prima classe lettura

--- Progetto/src/progetto/LetturaOggetti.java
+++ Progetto/src/progetto/LetturaOggetti.java
@@ -7,13 +7,36 @@ public class LetturaOggetti
     private String nomefile;
     private BufferedReader inputStream;
     
-    public LetturaOggetti(String nfile)
+    public LetturaOggetti()
     {
-        nomefile=nfile;
+        nomefile="Oggetti.txt";
+        inputStream=null;
     }
     
     public ArrayList<Oggetto> lettura()
     {
+        try
+        {
+            inputStream=new BufferedReader(new FileReader(nomefile));
+        }
+        catch(FileNotFoundException e)
+        {
+            System.exit(0);                    
+        }
+        
+        try
+        {
+            String stringa=inputStream.readLine();
+            while(stringa!=null)
+            {
+                
+            }
+            inputStream.close();
+        }
+        catch(IOException e)
+        {
+            System.exit(0);
+        }
         return null;
     }
     
