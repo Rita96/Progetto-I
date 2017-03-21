14de5f3026109b4766a15cb20f8e8e4a809d7d89
cl427862 <danitava@libero.it>
2017-03-21 16:33:57 +0100

    Modifica attributi classi Oggetto-Progetto.

--- Progetto/src/progetto/Progetto.java
+++ Progetto/src/progetto/Progetto.java
@@ -5,14 +5,10 @@ import java.util.ArrayList;
 public class Progetto 
 {
     private Oggetto oggetto;
-    private ArrayList<Scelta> scelte;
-    private ArrayList<Domanda> domande;
 
-    public Progetto(Oggetto o, ArrayList<Domanda> d, ArrayList<Scelta> s)
+    public Progetto(Oggetto o)
     {
         oggetto=o;
-        domande=d;
-        scelte=s;
     }
 
     public Oggetto getOggetto()
@@ -24,12 +20,12 @@ public class Progetto
     public String toString()
     {
         String string=null;
-        
+        int i=0;
         System.out.println("OGGETTO: "+oggetto.getNome()+" "+oggetto.getAttributo());
         System.out.println();
         for(Domanda d:oggetto.getDomande())
         {
-            System.out.println("DOMANDA: "+d.getCodD()+" "+d.getTestoD());
+            System.out.println("DOMANDA "+(i+1)+": "+d.getCodD()+" "+d.getTestoD());
             for(Scelta s:d.getScelteD())
             {
                 System.out.println("SCELTA: "+s.getCodS()+" "+s.getTestoS());
@@ -39,6 +35,7 @@ public class Progetto
                 System.out.println("ADIACENZA: "+adiacenza.getCodD());
             }
             System.out.println();
+            i++;
         }
         return string;
     }
