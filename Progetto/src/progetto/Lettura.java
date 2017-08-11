ef820d380419675417f964431e79b6ba487ea1e7
Pippo <aaff00@libero.it>
2017-08-11 09:53:16 +0200

    creata nuova superclasse Lettura per raggruppare parti comuni dei metodi lettura per le classi di lettura da file

---
+++ Progetto/src/progetto/Lettura.java
@@ -0,0 +1,32 @@
+package progetto;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Lettura {
+    
+    String nomeFile;
+    BufferedReader inputStream;
+
+    public Lettura(String nomeFile) 
+    {
+        this.nomeFile = nomeFile;
+        inputStream = null;
+    }
+    
+    public BufferedReader bufferedReaderInitialization()
+    {
+        try
+        {
+            inputStream = new BufferedReader(new FileReader(nomeFile));
+            return inputStream;
+        }
+        catch(FileNotFoundException e)
+        {
+            System.exit(0);
+        }
+        return null;
+    }
+    
+}
