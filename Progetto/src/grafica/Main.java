1193a00caec6cedbc1f7be2fa798fd5194e22dd5
Gabriele Guazzardi <gabrieleguazzardi@gmail.com>
2017-09-08 11:29:44 +0200

    Creazione pagina iniziale

--- Progetto/src/grafica/Main.java
+++ Progetto/src/grafica/Main.java
@@ -6,9 +6,6 @@ public class Main
 {
     public static void main(String[] args) throws IOException 
     {
-        ThreadGrafica thread = new ThreadGrafica();
-        thread.start();
-        ThreadGrafica thread2 = new ThreadGrafica();
-        thread2.start();
+        Home home = new Home();
     }
 }
